222a531a295bd652da94447f1ad8a929b1a6918d
itelman <ilyastelman02@gmail.com>
2024-07-23 20:24:26 +0500

    commit

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -10,6 +10,13 @@ import (
 
 var store sync.Map
 
+func ShowAllUsersHandler(w http.ResponseWriter, r *http.Request)  {}
+func CreateUserHandler(w http.ResponseWriter, r *http.Request)    {}
+func ShowUserHandler(w http.ResponseWriter, r *http.Request)      {}
+func UpdateUserHandler(w http.ResponseWriter, r *http.Request)    {}
+func DeleteUserHandler(w http.ResponseWriter, r *http.Request)    {}
+func ShowUserTasksHandler(w http.ResponseWriter, r *http.Request) {}
+
 // deleteTaskHandler godoc
 //
 //	@Summary		Delete a task
